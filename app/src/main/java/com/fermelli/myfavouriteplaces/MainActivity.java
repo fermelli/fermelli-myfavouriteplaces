918035eef58da6e391c2e9e16555cb507bca1ce3
fermelli <luisfernandosalgadomiguez@gmail.com>
2022-05-17 22:37:31 -0400

    Log out

--- app/src/main/java/com/fermelli/myfavouriteplaces/MainActivity.java
+++ app/src/main/java/com/fermelli/myfavouriteplaces/MainActivity.java
@@ -1,8 +1,16 @@
 package com.fermelli.myfavouriteplaces;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonCloseSession = findViewById(R.id.buttonCloseSession);
+
+        buttonCloseSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AuthUI.getInstance().signOut(MainActivity.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        MainActivity.this.finish();
+                    }
+                });
+
+            }
+        });
     }
 }
